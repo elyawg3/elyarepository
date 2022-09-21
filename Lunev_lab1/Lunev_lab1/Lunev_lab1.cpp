8bfad9dcdf759f68f1f514db7f99655d0e43f208
elyawg3 <elisey.lunev03@mail.ru>
2022-09-21 14:58:12 +0300

    Загружаем проект в репозиторий

---
+++ Lunev_lab1/Lunev_lab1/Lunev_lab1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void menuprint() {
+	cout << "1. Добавить трубу\n";
+	cout << "2. Добавить КС\n";
+	cout << "3. Просмотр всех объектов\n";
+	cout << "4. Редактировать трубу\n";
+	cout << "5. Редактировать КС\n";
+	cout << "6. Сохранить\n";
+	cout << "7. Загрузить\n";
+	cout << "0. Выход\n";
+}
+
+void 
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int num_menu;
+	menuprint();
+	cout << "Введите номер пункта: ";
+	cin >> num_menu;
+	switch (num_menu) {
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	case 7:
+		break;
+	case 0:
+		break;
+	default:
+
+
+	}
+	return 0;
+}
+
+
